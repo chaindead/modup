fe96305337458ad806bd4a2fb401e37274804f35
chaindead <miromaxxs@gmail.com>
2025-08-19 23:22:45 +0300

    feat: better text ui, version support

--- main.go
+++ main.go
@@ -10,8 +10,18 @@ import (
 	"github.com/chaindead/modup/internal/tui"
 )
 
+var (
+	buildTag    = "dev"
+	showVersion = pflag.BoolP("version", "v", false, "show version")
+)
+
 func main() {
 	pflag.Parse()
+
+	if *showVersion {
+		fmt.Println("modup", buildTag)
+		os.Exit(0)
+	}
 
 	if _, err := tea.NewProgram(tui.NewModel()).Run(); err != nil {
 		fmt.Println("Error running program:", err)
